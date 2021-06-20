d42698ee871a2a5946d42c76a566df4ade7d62c1
Nicolas DENDAUW <ndendauw.nd@gmail.com>
2021-06-20 16:43:01 +0200

    Replace boilerplate by knight

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -2,7 +2,7 @@
  * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
  * @extends {Actor}
  */
-export class BoilerplateActor extends Actor {
+export class knightActor extends Actor {
 
   /**
    * Augment the basic actor data with additional dynamic data.
